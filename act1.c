3862a109c788f92f6696ab088db3f62e32cd856e
Efrén Cayuela <151732101+efrencj@users.noreply.github.com>
2025-01-13 18:06:47 +0100

    Update act1.c

--- act1.c
+++ act1.c
@@ -22,5 +22,37 @@ void display_grid(int rowss, int cols, int grid[rows][cols]){
 }
 
 int count_neighbors(int rows, int cols, int grid[rows][cols], int x, int y){
-    
+    int contador=0;
+    for(int i=-1;i<=1;i++){
+        for(int j=-1; j<=1;j++){
+            if(i==0&&j==0) continue;
+            int fila = (x+i+rows) % rows;
+            int col = (y+j+cols) % cols;
+            contador+=grid[fila][col];
+        }
+    }
+    return contador;
+}
+
+void next_generation(int rows, int cols, int current[rows][cols], int next[rows][cols]){
+    for(int i=0;i<rows;i++){
+        for(int j=0;i<cols;j++){
+            int quants=count_neighbors(rows, cols, current, i, j);
+            if(current[i][j]==1){
+                next[i][j] = (quants==2||quants==3) ? 1 :0;//sup
+            } else{
+                next[i][j] = (quants==3)?1:0;//naixement
+            }
+        }
+    }
+}
+
+int grids_are_equal(int rows, int cols, int grid1[rows][cols], int grid2[rows][cols]){
+    for(int i=0;i<rows;i++){
+        for(int j=0; j<cols;j++){
+            if(grid1[i][j] != grid2[i][j]){
+                return 0;//no son iguals
+            }
+        }
+    }
 }
