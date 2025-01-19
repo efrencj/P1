d4e74eadc5cd84ba1e4111058d36e8ce91854f7f
Efrén Cayuela <151732101+efrencj@users.noreply.github.com>
2025-01-19 16:51:51 +0100

    Update act1.c

--- act1.c
+++ act1.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 
+void initialize_grid(int rows, int cols, int grid[rows][cols]);
+void display_grid(int rows, int cols, int grid[rows][cols]);
+
+
+
 void initialize_grid(int rows, int cols, int grid[rows][cols]){
     srand(time(NULL)); //for generate a random number
     for(int i=0; i<rows; i++){
@@ -11,7 +16,7 @@ void initialize_grid(int rows, int cols, int grid[rows][cols]){
     }
 }
 
-void display_grid(int rowss, int cols, int grid[rows][cols]){
+void display_grid(int rows, int cols, int grid[rows][cols]){
     for(int i=0; i<rows;i++){
         for(int j=0; j<cols; j++){
             printf("%c ", grid[i][j] ? '#':'.');
@@ -21,38 +26,18 @@ void display_grid(int rowss, int cols, int grid[rows][cols]){
     printf("\n");
 }
 
-int count_neighbors(int rows, int cols, int grid[rows][cols], int x, int y){
-    int contador=0;
-    for(int i=-1;i<=1;i++){
-        for(int j=-1; j<=1;j++){
-            if(i==0&&j==0) continue; // ALEX: esto es legal? creo que se puede mejorar con un booleano
-            int fila = (x+i+rows) % rows;
-            int col = (y+j+cols) % cols;
-            contador+=grid[fila][col];
-        }
+int main(int argc, char *argv[]){
+    if(argc!=3){
+        return 1;
     }
-    return contador;
+    int rows = atoi(argv[1]);
+    int cols = atoi(argv[2]);
+    if (rows <= 0 || cols <= 0) {
+        printf("Rows and cols must be more than 0.\n");
+        return 1;
+    }
+    int grid[rows][cols];
+    initialize_grid(rows, cols, grid);
+    display_grid(rows, cols, grid);
 }
 
-void next_generation(int rows, int cols, int current[rows][cols], int next[rows][cols]){
-    for(int i=0;i<rows;i++){
-        for(int j=0;i<cols;j++){
-            int quants=count_neighbors(rows, cols, current, i, j); 
-            if(current[i][j]==1){
-                next[i][j] = (quants==2||quants==3) ? 1 :0;//sup
-            } else{
-                next[i][j] = (quants==3)?1:0;//naixement
-            }
-        }
-    }
-}
-
-int grids_are_equal(int rows, int cols, int grid1[rows][cols], int grid2[rows][cols]){
-    for(int i=0;i<rows;i++){
-        for(int j=0; j<cols;j++){
-            if(grid1[i][j] != grid2[i][j]){
-                return 0;//no son iguals
-            }
-        }
-    }
-}
